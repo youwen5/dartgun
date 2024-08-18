7aec3310daac1f3065f16ad9e7f16225a999b993
Youwen Wu <youwenw@gmail.com>
2024-08-18 14:49:07 -0700

    devel: add flake and envrc for consistent dev environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for dartgun development environment.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
